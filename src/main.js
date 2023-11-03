ce2e81c814123d824865328a081d227a507e3ec6
Barry-san <oyeyemimubarak6@gmail.com>
2023-11-03 14:18:56 +0100

    commiting before merge

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import LocomotiveScroll from "locomotive-scroll";
-
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-  smartphone: {
-    smooth: false,
-  },
-});
